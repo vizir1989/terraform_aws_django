c95e1c133d4f868136fb6820b3736166a958570f
vladislav.ninchuk <vizir.vs@gmail.com>
2023-03-10 00:47:56 +0400

    added auto create admin

--- terraform/09_auto_scaling.tf
+++ terraform/09_auto_scaling.tf
@@ -1,8 +1,8 @@
 resource "aws_autoscaling_group" "ecs-cluster" {
   name                 = "${terraform.workspace}_auto_scaling_group"
   min_size             = terraform.workspace == "prod" ? var.autoscale_min : 1
-  max_size             = terraform.workspace == "prod" ? var.autoscale_max : 1
-  desired_capacity     = terraform.workspace == "prod" ? var.autoscale_desired : 1
+  max_size             = terraform.workspace == "prod" ? var.autoscale_max : 4
+  desired_capacity     = terraform.workspace == "prod" ? var.autoscale_desired : 2
   health_check_type    = "EC2"
   launch_configuration = aws_launch_configuration.ecs.name
   vpc_zone_identifier  = [aws_subnet.private-subnet-1.id, aws_subnet.private-subnet-2.id]
