97d59ee71ab4e67caf7fc435f3678665ae5064d1
vladislav.ninchuk <vizir.vs@gmail.com>
2023-03-09 23:59:26 +0400

    added auto create admin

--- app/scripts/run_server.sh
+++ app/scripts/run_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
+python manage.py migrate
 python manage.py createsuperuser --no-input
 gunicorn -w 3 -b :8000 terraform_aws_django.wsgi:application
